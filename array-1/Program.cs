dceb6e1d423f6763c69e9dd51577f35c2918288d
Guilhlm <141831938+Guilhlm@users.noreply.github.com>
2023-09-13 11:04:36 -0300

    Add files via upload

---
+++ array-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] nome = { "gui", "pedro", "renon","edilson","monique", "gustavo" };
+            float[] idade = { 17, 25, 19, 19, 18, 29 };
+
+            float sominha = 0;
+
+            Array.ForEach(idade, i => sominha += i / 6);
+            
+            for (int i = 0; i < nome.Length && i < idade.Length; i++)
+            {
+                Console.WriteLine(nome[i]);
+                Console.WriteLine(idade[i]);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("");
+            Console.WriteLine("media das idades");
+            Console.WriteLine(sominha);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
