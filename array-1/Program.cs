f1f09883461923aea82164c670ef1de2e4d24ea5
Guilhlm <141831938+Guilhlm@users.noreply.github.com>
2023-09-13 11:14:06 -0300

    Add files via upload

--- array-1/Program.cs
+++ array-1/Program.cs
@@ -11,27 +11,30 @@ namespace array_1
         static void Main(string[] args)
         {
 
-            string[] nome = { "gui", "pedro", "renon","edilson","monique", "gustavo" };
-            float[] idade = { 17, 25, 19, 19, 18, 29 };
+            string[] nome = { "gui", "pedro", "renon","edilson","monique", "gustavo" }; //tipo da variavel [] - array variavel { };
 
-            float sominha = 0;
+            float[] idade = { 17, 25, 19, 19, 18, 29 }; //tipo da variavel [] - array variavel { };
 
-            Array.ForEach(idade, i => sominha += i / 6); //foreach executa tudo dentro da array
+            float sominha = 0; //variavel com valor fixo;
+
+            Array.ForEach(idade, i => sominha += i / 6); //foreach executa tudo dentro da array;
             
-            for (int i = 0; i < nome.Length && i < idade.Length; i++) // variavel.LENGTH soma tudo da array
+            for (int i = 0; i < nome.Length && i < idade.Length; i++) // variavel.LENGTH soma tudo da array   // i= contador
             {
-                Console.WriteLine(nome[i]);
-                Console.WriteLine(idade[i]);
+                Console.WriteLine(nome[i]); // (variavel[i=contagem]);
+
+                Console.WriteLine(idade[i]); // (variavel[i=contagem]);
             }
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("");
-            Console.WriteLine("media das idades");
-            Console.WriteLine(sominha);
+            Console.ForegroundColor = ConsoleColor.Red; //corzinha;
 
+            Console.WriteLine(""); //espaço de baiano;
 
+            Console.WriteLine("media das idades"); //escreve texto;
 
-            Console.ReadKey();
+            Console.WriteLine(sominha); //exibe o valor da minha variavel;
+
+            Console.ReadKey(); //mantem o console ativo ;
         }
     }
 }
